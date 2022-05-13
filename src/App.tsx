d1b71941aeef42a72fd99ff83ae605325e0ef5f6
unknown <Student@108-24.vssdomena.scv.si>
2022-05-13 12:58:45 +0200

    Moj prvi ace

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Card from "./components/Footer";
+import Footer from "./components/Footer";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+<Nav></Nav>
+        <main>
+
+            <Welcome></Welcome>
+
+            <div className="album py-5 bg-light">
+                <div className="container">
+
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        <div className="col">
+                            <div className="card shadow-sm">
+                                <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+                                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                     preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                                    <rect width="100%" height="100%" fill="#55595c"/>
+                                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                                </svg>
+
+                                <div className="card-body">
+                                    <p className="card-text">Enis je ful vlka kesa. Slab je u basketu pa isto navija za miami. Shooting bricks</p>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <div className="btn-group">
+                                            <button type="button" className="btn btn-sm btn-outline-secondary">View
+                                            </button>
+                                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit
+                                            </button>
+                                        </div>
+                                        <small className="text-muted">9 mins</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </main>
+
+        <Footer></Footer>
+
+
+    </>
   );
 }
 
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Welcome = () => {
+    return(
+        <>
+            <section className="py-5 text-center container">
+                <div className="row py-lg-5">
+                    <div className="col-lg-6 col-md-8 mx-auto">
+                        <h1 className="fw-light">Mama ti je traktor</h1>
+                        <p className="lead text-muted">Enis je ful vlka kesa. Slab je u basketu pa isto navija za miami.
+                            Shooting bricks</p>
+                        <p>
+                            <a href="#" className="btn btn-primary my-2">Main call to action</a>
+                            <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default Welcome;
